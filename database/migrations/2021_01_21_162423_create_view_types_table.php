b69fb0b1d9fc68cce9e6b38f58bb3585454a3c7b
George Pipkin <gpp8p@Virginia.EDU>
2021-01-21 11:47:17 -0500

    Setup of database - checked out with shandev ok

---
+++ database/migrations/2021_01_21_162423_create_view_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViewTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('view_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('view_type_label',32);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('view_types');
+    }
+}
